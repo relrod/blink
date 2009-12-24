91d30e72112ad67bbf5ed5244b8f88a503c7174a
James S <james@nixeagle.org>
2009-12-24 14:39:56 +0800

    so much for last commit of the night, added check_seen_messages, using this will give blink a memory

--- Grab.pm
+++ Grab.pm
@@ -8,11 +8,31 @@ use strict;
 package Grab;
 use DBI;
 use Data::Dumper;
+
+#module local hash to track channels->users = messages. This is used by
+#fetch_grab_row and other primatives
+my $last_seen_messages = {};
+
 my $dbh = DBI->connect("dbi:SQLite:dbname=db_blink.sqlite3","","",{AutoCommit=>1,PrintError=>1});
 
 #prep for queries
 my $new_grab = $dbh->prepare("INSERT INTO grabs(by,who,message,channel) VALUES(?,?,?,?)");
 my $fetch_grab = $dbh->prepare("SELECT message FROM grabs WHERE who=? AND channel=? ORDER BY RANDOM()");
+
+#return 1 or 0 depending on if the message is new or not.  failure to
+#verify that the user has more then one message will cause an infinate
+#loop if the only check is done with this.
+
+sub check_seen_messages {
+  my ($who, $channel, $message) = @_;
+  if( exists $last_seen_messages->{$channel}{$who} and
+      $last_seen_messages->{$channel}{$who} eq $message) {
+    return 0;
+  } else { #update the hash and return 1
+    $last_seen_messages->{$channel}{$who} = $message;
+    return 1;
+  }
+}
 
 #debugging function
 sub fetch_grab_row {
