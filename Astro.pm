93a2aaa9b698c66b7216b8f568cde23ca44765cf
Rick E <ricky@lightning.local>
2010-01-24 22:18:20 -0500

    new weather module. Completely re-written and uses Weather::Google now.

--- Astro.pm
+++ Astro.pm
@@ -6,42 +6,26 @@ use warnings;
 use strict;
 
 package Astro;
-use Weather::Underground;
+use Weather::Google;
 use Data::Dumper;
 
 sub fetchweather {
 	my $location = shift;
-	my $wug = Weather::Underground->new(
-		place => $location,
-		debug => 0
-	);
-	my $weatherinfo = $wug->get_weather;
-	if(defined($weatherinfo)){
-		# Get all the vars we're going to use and put them in
-		# a non-evil form (i.e so we can use them without
-		# concatting strings.
-      my $windresponse;
-		my $visibility = $weatherinfo->[0]->{'visibility_miles'};
-		my $winddir = $weatherinfo->[0]->{'wind_direction'};
-		my $windmph = $weatherinfo->[0]->{'wind_milesperhour'};
-      if($windmph eq "0.0"){
-         $windresponse = "Wind is nonexistant, at 0.0 MPH";
-      } else {
-         $windresponse = "Wind is $winddir at $windmph MPH";
-      }
-		my $place = $weatherinfo->[0]->{'place'};
-		my $tempf = $weatherinfo->[0]->{'temperature_fahrenheit'};
-		my $tempc = $weatherinfo->[0]->{'celsius'};
-		my $humidity = $weatherinfo->[0]->{'humidity'};
-      $humidity = "unknown" if $humidity == "999";
-		my $conditions = $weatherinfo->[0]->{'conditions'};
-                $conditions = 'Clear Sky' if $conditions eq 'Clear';
-		return "Weather for ($place): Currently, this location is experiencing $conditions conditions. "
-			." $windresponse. Temperature is "
-			.$tempf."F (".$tempc."C), and humidity is $humidity%. Visibility is $visibility miles.";
-	} else {
-		return "Sorry, but weather for '$location' could not be retrieved.";
-	}
+   my $wxobj = Weather::Google->new($location);
+   my $cond = $wxobj->current();
+   if(exists($cond->{"condition"}) != 1) {
+      return "Sorry, conditions for '$location' could not be found.";
+   } else {
+      my $city = $wxobj->{"info"}->{"city"};
+      # And throw it into some vars.
+      my $humidity = $cond->{"humidity"};
+      my $tempf = $cond->{"temp_f"};
+      my $tempc = $cond->{"temp_c"};
+      my $wind = $cond->{"wind_condition"};
+      my $conditions = $cond->{"condition"};
+      return 
+         "Conditions for $city: $conditions. $humidity -- $tempf°F ($tempc°C) -- $wind.";
+   }
 }
 
 1; # Make perl happy.
